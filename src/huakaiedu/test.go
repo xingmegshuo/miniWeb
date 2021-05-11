f63d4c4b6adf03da17140d0b263b9c8bed733d18
ant <ant@antdeMac-mini.local>
2021-05-11 19:34:23 +0800

    update

--- src/huakaiedu/test.go
+++ src/huakaiedu/test.go
@@ -11,7 +11,6 @@ package main
 import (
 	"huakai/handler"
 	"huakai/models"
-	"huakai/pagination"
 	"log"
 )
 
@@ -20,14 +19,18 @@ var db = handler.DB
 func main() {
 	log.Println("hello")
 	var tables []models.JoinUs
-	var count int64
-	query := &pagination.PaginationQuery{
-		PageSize: 10,
-		PageNum:  1,
-		OrderBy:  "id",
-		Order:    "asc",
-	}
-	db.Model(&models.JoinUs{}).Scopes(pagination.PaginationScope(query)).Count(&count)
-	db.Model(&models.JoinUs{}).Scopes(pagination.PaginationScope(query)).Find(&tables)
+	//var count int64
+	//query := &pagination.PaginationQuery{
+	//	PageSize: 10,
+	//	PageNum:  1,
+	//	OrderBy:  "id",
+	//	Order:    "asc",
+	//}
+	//log.Print(pagination.PaginationScope(query))
+	//db.Model(&models.JoinUs{}).Scopes(pagination.PaginationScope(query)).Count(&count)
+	//db.Model(&models.JoinUs{}).Scopes(pagination.PaginationScope(query)).Find(&tables)
+
+	db.Preload("user").Preload("user.id").Where("join_us.user_id = ?", 2).Find(&tables)
 	log.Println(tables)
+
 }
